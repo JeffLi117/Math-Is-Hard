ec62be552fa24fa3b52813dce94430a6f5c29c28
Jeffrey Li <li.jeffreyt@yahoo.com>
2022-11-11 11:52:26 -0600

    Added functionality to 0-9 and all calculation buttons; can now continue calcs after using equals sign; needs decimal implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Math-Is-Hard
+
+This is my calculator project that is supposed to replicate a very simple calculator. Not a TI-84, but old-school cool. No inputting long calculations with multiple functions and parentheses! It works with only two numbers at a time. Hope you enjoy it.
--- math.js
+++ math.js
@@ -1,3 +1,10 @@
+let storedString1 = "";
+let storedNumber1 = 0;
+let storedFunc = "";
+let lastButtonClicked = "start";
+let hasCalcBeenPressed = "no";
+let equalsJustClicked = "no";
+
 function add(a, b) {
     return a+b}
 
@@ -14,23 +21,20 @@ function divide(a,b) {
 function operate(operator, number1, number2) {
     return operator(number1, number2)}   
 
-let storedString1 = "";
-let storedNumber1 = 0;
-let storedFunc = "";
-let lastButtonClicked = "";
-
 let container = document.querySelector('container');
 container = document.createElement('div');
 container.id = "main"
 
 let display = document.createElement('div');
 display.className = "display";
-display.textContent = "80085";
+display.textContent = "0";
 
 document.getElementById('container').appendChild(display);
 
 buttonClear.addEventListener('click', (e) => {
     display.textContent = "0";
+    lastButtonClicked = "start";
+    hasCalcBeenPressed = "no";
 })
 
 /* storedNumber1 != "" meaning a number has been entered and a calc button has been pressed 
@@ -39,7 +43,7 @@ lastButtonClicked = "num" makes it so that it will not reset display and will co
 button0.addEventListener('click', (e) => {
     if (display.textContent === "0" || lastButtonClicked === "calc") {
         display.textContent = "0";
-    } else if (lastButtonClicked === "num") {
+    } else {
     display.textContent += "0";
     }
     lastButtonClicked = "num";
@@ -51,10 +55,8 @@ button1.addEventListener('click', (e) => {
     } else if (lastButtonClicked === "calc") {
         display.textContent = "";
         display.textContent += "1";
-        console.log(display.textContent);
-    } else if (lastButtonClicked === "num") {
+    } else {
         display.textContent += "1";
-        console.log(display.textContent);
     }
     lastButtonClicked = "num";
 })
@@ -65,82 +67,189 @@ button2.addEventListener('click', (e) => {
     }  else if (lastButtonClicked === "calc") {
         display.textContent = "";
         display.textContent += "2";
-        console.log(display.textContent);
-    } else if (lastButtonClicked === "num") {
+    } else {
         display.textContent += "2";
-        console.log(display.textContent);
     }
     lastButtonClicked = "num";
 })
 
 button3.addEventListener('click', (e) => {
-    display.textContent += "3";
+    if (display.textContent === "0") {
+        display.textContent = "3";
+    } else if (lastButtonClicked === "calc") {
+        display.textContent = "";
+        display.textContent += "3";
+    } else {
+        display.textContent += "3";
+    }
+    lastButtonClicked = "num";
 })
 
 button4.addEventListener('click', (e) => {
-    display.textContent += "4";
+    if (display.textContent === "0") {
+        display.textContent = "4";
+    } else if (lastButtonClicked === "calc") {
+        display.textContent = "";
+        display.textContent += "4";
+    } else {
+        display.textContent += "4";
+    }
+    lastButtonClicked = "num";
 })
 
 button5.addEventListener('click', (e) => {
-    display.textContent += "5";
+    if (display.textContent === "0") {
+        display.textContent = "5";
+    } else if (lastButtonClicked === "calc") {
+        display.textContent = "";
+        display.textContent += "5";
+    } else {
+        display.textContent += "5";
+    }
+    lastButtonClicked = "num";
 })
 
 button6.addEventListener('click', (e) => {
-    display.textContent += "6";
+    if (display.textContent === "0") {
+        display.textContent = "6";
+    } else if (lastButtonClicked === "calc") {
+        display.textContent = "";
+        display.textContent += "6";
+    } else {
+        display.textContent += "6";
+    }
+    lastButtonClicked = "num";
 })
 
 button7.addEventListener('click', (e) => {
-    display.textContent += "7";
+    if (display.textContent === "0") {
+        display.textContent = "7";
+    } else if (lastButtonClicked === "calc") {
+        display.textContent = "";
+        display.textContent += "7";
+    } else {
+        display.textContent += "7";
+    }
+    lastButtonClicked = "num";
 })
 
 button8.addEventListener('click', (e) => {
-    display.textContent += "8";
+    if (display.textContent === "0") {
+        display.textContent = "8";
+    } else if (lastButtonClicked === "calc") {
+        display.textContent = "";
+        display.textContent += "8";
+    } else {
+        display.textContent += "8";
+    }
+    lastButtonClicked = "num";
 })
 
 button9.addEventListener('click', (e) => {
-    display.textContent += "9";
+    if (display.textContent === "0") {
+        display.textContent = "9";
+    } else if (lastButtonClicked === "calc") {
+        display.textContent = "";
+        display.textContent += "9";
+    } else {
+        display.textContent += "9";
+    }
+    lastButtonClicked = "num";
 })
-console.log(lastButtonClicked);
 
 calcAdd.addEventListener('click', (e) => {
+    if (lastButtonClicked === "start") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    } else if (equalsJustClicked === "yes") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+        equalsJustClicked = "no";
+    } else if (hasCalcBeenPressed === "no") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    } else {
+        display.textContent = operate(storedFunc, storedNumber1, Number(display.textContent));
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    }
     storedFunc = add;
-    storedString1 = display.textContent;
-    storedNumber1 = Number(storedString1);
     lastButtonClicked = "calc";
-    console.log(lastButtonClicked);
-    console.log(storedNumber1);
+    hasCalcBeenPressed = "yes";
 })
 
-//clicking on numbers adds to display.textContent and stores each inputted number on said display into storedNumber1
-//click on calculation symbol needs to store function into storedFunc (?and move storedNumber1 into storedNumber2?)
-//next click on any number should reset display.textContent to "" and then fill as it did previously
-//current number on display.textContent 
-
-
-
-/* Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step. */
-
-
-
-/* let belowDisplay = document.createElement('div');
-belowDisplay.className = "belowDisplay";
-belowDisplay.textContent = "test";
-
-function numberGrid(t) {
-    for (i = 0; i < 3; i++) {
-        let row = document.createElement('div');
-        row.className = "row";
-        row.id = "row" + i;
-
-        for (j = 0; j < 3; j++) {
-            let box = document.createElement('div');
-            box.className = "box";
-            box.id = "box" + i;
-            row.appendChild(box);
-        }
-        container.appendChild(row);
+calcSubtract.addEventListener('click', (e) => {
+    if (lastButtonClicked === "start") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    } else if (equalsJustClicked === "yes") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+        equalsJustClicked = "no";
+    } else if (hasCalcBeenPressed === "no") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    } else {
+        display.textContent = operate(storedFunc, storedNumber1, Number(display.textContent));
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
     }
-    t.appendChild(container)
-}
-document.body.appendChild(belowDisplay);
-numberGrid(document.body); */
+    storedFunc = subtract;
+    lastButtonClicked = "calc";
+    hasCalcBeenPressed = "yes";
+})
+
+calcMultiply.addEventListener('click', (e) => {
+    if (lastButtonClicked === "start") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    } else if (equalsJustClicked === "yes") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+        equalsJustClicked = "no";
+    } else if (hasCalcBeenPressed === "no") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    } else {
+        display.textContent = operate(storedFunc, storedNumber1, Number(display.textContent));
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    }
+    storedFunc = multiply;
+    lastButtonClicked = "calc";
+    hasCalcBeenPressed = "yes";
+})
+
+calcDivide.addEventListener('click', (e) => {
+    if (lastButtonClicked === "start") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    } else if (equalsJustClicked === "yes") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+        equalsJustClicked = "no";
+    } else if (hasCalcBeenPressed === "no") {
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    } else {
+        display.textContent = operate(storedFunc, storedNumber1, Number(display.textContent));
+        storedString1 = display.textContent;
+        storedNumber1 = Number(storedString1);
+    }
+    storedFunc = divide;
+    lastButtonClicked = "calc";
+    hasCalcBeenPressed = "yes";
+})
+
+calcEquals.addEventListener('click', (e) => {
+    if (hasCalcBeenPressed === "no" || lastButtonClicked === "calc") {
+        return;
+    } else {
+        display.textContent = operate(storedFunc, storedNumber1, Number(display.textContent));
+        lastButtonClicked = "calc";
+        hasCalcBeenPressed = "yes";
+        equalsJustClicked = "yes";
+    }
+})
+
+
